c9bca807ff370a5373981f0db8ae659af33fc9da
Bart Louwers <bart@emeel.net>
2022-12-22 21:01:41 +0100

    redirect /sticker

---
+++ src/pages/sticker.page.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+function redirect() {
+    if (typeof window === 'undefined')
+        return
+    location.href = "/#uitleg";
+
+}
+
+redirect();
+</script>
